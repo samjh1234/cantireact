362a77ac2068be0c451c5b383087ed383e329cf2
samjh1234 <wisamjh@hotmail.com>
2024-12-16 23:24:35 +0100

    Updates

---
+++ precache-manifest.e98821548b94353b3f2f056aec52283e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdbed700cb163b07c4b6831bdf6bb0ff",
+    "url": "/cantireact/index.html"
+  },
+  {
+    "revision": "b236cc65694fd47295e9",
+    "url": "/cantireact/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "91532fe74c9ded204142",
+    "url": "/cantireact/static/js/2.a7f37d0f.chunk.js"
+  },
+  {
+    "revision": "d5a2140746d4a4e5c89e",
+    "url": "/cantireact/static/js/3.e13311ee.chunk.js"
+  },
+  {
+    "revision": "b236cc65694fd47295e9",
+    "url": "/cantireact/static/js/main.bf9fdce2.chunk.js"
+  },
+  {
+    "revision": "03c84b4abfaa9ab4962f",
+    "url": "/cantireact/static/js/runtime~main.93bfc1f6.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/cantireact/static/media/logo.06e73328.svg"
+  }
+]);

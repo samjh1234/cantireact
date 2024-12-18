36edc20f1c4c873966f1d2766efa024dfb3b287c
samjh1234 <wisamjh@hotmail.com>
2024-12-18 12:10:16 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Aggiungi from './components/Aggiungi';
 
 function App() {
   return (
-    <Router basename="/cantireact"> 
-      <Routes>
-        <Route path="/" element={<Home />} /> 
-        <Route path="/aggiungi" element={<Aggiungi />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/aggiungi" element={<Aggiungi />} />
+    </Routes>
   );
 }
 
